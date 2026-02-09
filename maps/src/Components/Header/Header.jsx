ede5334acb305e2ce29ba8e26591d480ec9a333a
Mario Esteban Espitia Martinez <71477601+marckes04@users.noreply.github.com>
2026-02-09 21:52:14 +0100

    Header added

---
+++ maps/src/Components/Header/Header.jsx
@@ -0,0 +1,64 @@
+import { AppBar, Toolbar, Typography, InputBase, Box, alpha, styled } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
+
+// This replaces the old useStyles hook
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(3),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
+
+const Header = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Typography variant="h5">Travel Advisor</Typography>
+        <Box display="flex" alignItems="center">
+          <Typography variant="h6">Explore new places</Typography>
+          {/* Use Autocomplete here once the API is loaded */}
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase placeholder="Search…" />
+          </Search>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
